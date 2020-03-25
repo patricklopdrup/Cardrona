73e962679586c7527f88acbf9c8060f3650b17f7
Patrick <patrick.lopdrup@gmail.com>
2020-03-25 09:44:31 +0100

    start på inplementation af array

--- Game.py
+++ Game.py
@@ -1,1 +1,24 @@
-print("hej")
+import numpy as np
+
+# Easy access for the "data" array
+CARD_DECK = 0
+TURNED = 1
+TURNED_DECK = 2
+HEARTS = 3
+SPADES = 4
+DIAMONDS = 5
+CLUBS = 6
+
+# 2D array for the game. 7 rows of max 13 cards (ace to king)
+solitaire = np.zeros((7, 13), dtype=object)
+
+# Data array for other than game info
+data = ['', '', '', '', '', '', '']
+
+
+solitaire[1, 1] = "b"
+
+print(solitaire)
+
+data[1] = "2c"
+print(data)
