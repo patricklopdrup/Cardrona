c17d36ef778633559872fc9600730767ee481151
Patrick <patrick.lopdrup@gmail.com>
2020-03-25 14:55:09 +0100

    lavet en start_game() som lægger et spil ind i arrayet

--- card.py
+++ card.py
@@ -1,15 +1,23 @@
+import random
+
+
 class Card:
     def __init__(self):
         pass
 
     def make_card(self, number, suit):
-        return (str(number) + suit)
+        return str(number) + suit
 
     def make_deck(self):
+        # Hearts, Spades, Diamonds, Clubs
         suits = ["H", "S", "D", "C"]
         deck = []
         for suit in suits:
             # 13 cards for each suit
             for num in range(1, 14):
                 deck.append(self.make_card(num, suit))
+        return deck
+
+    def shuffle(self, deck):
+        random.shuffle(deck)
         return deck
