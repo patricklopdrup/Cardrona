70781a55ba437e6d60d53cfa4d039c53efe5270b
Patrick <patrick.lopdrup@gmail.com>
2020-04-15 08:18:59 +0200

    Auto stash before merge of "master" and "origin/master"
    kortene har nu en bagside og denne bliver vist i spillet, men kortene er de samme hele spillet igennem

--- Game.py
+++ Game.py
@@ -14,41 +14,63 @@ CLUBS = 6
 solitaire = np.zeros((7, 13), dtype=object)
 
 # Data array for other than game info
-data = ['', '', '', '', '', '', '']
-
+# data = ['', '', '', '', '', '', '']
+data = np.zeros((7), dtype=object)
+card_deck = np.zeros((24), dtype=object)
 
 deck = card.Deck()
 # card = card.Card()
 
 
+def draw_card():
+    print(f"kort: {data[CARD_DECK]}")
+    data[TURNED] = data[CARD_DECK]
+
+
 def start_game():
+    data[HEARTS] = 0
+    data[SPADES] = 0
+    data[DIAMONDS] = 0
+    data[CLUBS] = 0
+    data[TURNED_DECK] = 0
 
     m_deck = deck.make_deck()
-    print(f"længde: {len(m_deck)}")
+    print(f"Antal kort: {len(m_deck)}")
+    print(f"hej: {data[CARD_DECK]}")
     m_deck = deck.shuffle(m_deck)
-    back_counter = 0
 
     for row in range(7):
         for column in range(7):
 
             if column == row:
-                solitaire[row, column] = m_deck.pop(1)
+                solitaire[row, column] = m_deck.pop(0)
             elif column < row:
-                solitaire[row, column] = "[ ]"
-                back_counter += 1
-    data[CARD_DECK] = (len(m_deck) - back_counter)
-    print(solitaire)
-    print(data)
+                card = m_deck.pop(0)
+                card.flipped = True
+                solitaire[row, column] = card
+    # putting the rest of the card in card_deck
+    for i in range(len(m_deck)):
+        card_deck[i] = m_deck[i]
+    # saving the card_deck in the data array
+    data[CARD_DECK] = card_deck
+
+
+def show_card_deck():
+    for card in data[CARD_DECK]:
+        print(card, end=", ")
+    print()
+
+
+def turn_card():
+    top_card = data[CARD_DECK][-1]
+    data[CARD_DECK] = data[CARD_DECK]
+    data[TURNED] = top_card
 
 
 def show():
-    print(f"hej med dig: {solitaire[0,0]} er {solitaire[0,0].color}")
-    data[HEARTS] = 0
-    data[SPADES] = 0
-    data[DIAMONDS] = 0
-    data[CLUBS] = 0
+    print(f"Første kort: {solitaire[0,0]} er {solitaire[0,0].color}")
     print()
-    print(f"Deck: {data[CARD_DECK]}")
+    show_card_deck()
     print(f"Turned card: {data[TURNED]}")
     print(
         f"H:{data[HEARTS]} S:{data[SPADES]} D:{data[DIAMONDS]} C:{data[CLUBS]}")
@@ -56,7 +78,11 @@ def show():
         print()
         for row in range(7):
             if solitaire[row, column]:
-                print(solitaire[row, column], end=" ")
+                # print back-side of card if it's flipped - else print the card
+                if solitaire[row, column].flipped == True:
+                    print("[ ]", end=" ")
+                else:
+                    print(solitaire[row, column], end=" ")
             else:
                 print(" "*4, end="")
 
