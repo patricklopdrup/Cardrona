7e2f736077437df8a17ac39357fa82462b51d0e5
Martin Rylund <martin@rylund.eu>
2020-06-07 18:24:47 +0200

    Added support to export card rows into separate images
    This is needed due to our Darknet model only supporting smaller sized images, trying to use our Darknet model on a full sized image will result in a lot of false detections.

--- detection/card_detection.py
+++ detection/card_detection.py
@@ -1,19 +1,40 @@
 from os import path
+import numpy as np
+from pprint import pprint
 import cv2
 
 
 def get_rows(img):
+    # Get the grayscale of the image and reduce noise
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.bilateralFilter(gray, 11, 17, 17)
+
+    # Extract the edges
     edge = cv2.Canny(gray, 30, 200)
 
+    # Find the contours from the edged image
     contours, _ = cv2.findContours(edge.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-    cv2.imshow('GrayScale', gray)
-    cv2.imshow('Edges', edge)
-    cv2.imshow('Contours', img)
-    cv2.waitKey()
+    # Sort the found contours by their x position
+    sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    img_cnts = img.copy()
+
+    idx = 0
+    h, w, _ = img.shape
+    minArea = w / 10 * w / 10
+    for contour in sorted_contours:
+        area = cv2.contourArea(contour)
+        if area > minArea:
+            print(area)
+            idx += 1
+            x, y, w, h = cv2.boundingRect(contour)
+            roi = img_cnts[y:y + h, x:x + w]
+            cv2.imwrite(str(idx) + '.png', roi)
+            cv2.drawContours(img_cnts, [contour], -1, (0, 255, 0), 3)
+            cv2.imshow('Contours', roi)
+            cv2.waitKey()
+
     cv2.destroyAllWindows()
 
 
