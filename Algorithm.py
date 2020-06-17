7066310a828c17aa70756d9c8ad1656564e8c70d
Joe-Briefcase <s185143@student.dtu.dk>
2020-06-17 14:20:22 +0200

    Algoritmen virker.

--- Algorithm.py
+++ Algorithm.py
@@ -1,32 +1,34 @@
 import Agent
+import game_columns
 
-"""
-All return types need to be replaced by their equivalent method.
-"""
-def decision(moves):
+def decision(moves, current_card=None, card_destination=None):
     # Draw card if no moves available.
     if not moves:
         return "Træk et kort"
 
     # Check suit moves.
-    for i in len(moves):
-        if moves[i][0][1] == 7:
-            return "Ryk kort til første suit"
-        elif moves[i][0][1] == 8:
-            return "Ryk kort til andet suit"
-        elif moves[i][0][1] == 9:
-            return "Ryk kort til tredje suit"
-        elif moves[i][0][1] == 10:
-            return "Ryk kort til fjerde suit"
+    for i in range(len(moves)):
+        if moves[i][1][1] == 7:
+            return "Ryk " + str(moves[i][0][0].number) + str(moves[i][0][0].suit) + " til første suit"
+        elif moves[i][1][1] == 8:
+            return "Ryk " + str(moves[i][0][0].number) + str(moves[i][0][0].suit) + " til andet suit"
+        elif moves[i][1][1] == 9:
+            return "Ryk " + str(moves[i][0][0].number) + str(moves[i][0][0].suit) + " til tredje suit"
+        elif moves[i][1][1] == 10:
+            return "Ryk " + str(moves[i][0][0].number) + str(moves[i][0][0].suit) + " til fjerde suit"
     """
     Prioriteter:
     1. Bunker med flest kort med forsiden nedad.
     2. Hvis man rykker en kolonne så skal der prioriteres dem med konger.
     """
+    y_pos = 0
+    current_card
+    card_destination
     # Check regular moves.
-    for move in moves:
-        # TODO: Prioritér kort?
-        if move == "COLUMN":
-            return "MOVE COLUMN"
-        elif move == "ONE CARD":
-            return "MOVE CARD"
+    for i in range(len(moves)):
+        if moves[i][0][2] > y_pos:
+            y_pos = moves[i][0][2]
+            current_card = moves[i][0][0]
+            card_destination = moves[i][1][0]
+
+    return "Ryk " + str(current_card.number) + str(current_card.suit) + " til " + str(card_destination.number) + str(card_destination.suit)
