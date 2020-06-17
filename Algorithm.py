7729d55110c887c52a2720afe6414932cf7546d3
Joe-Briefcase <s185143@student.dtu.dk>
2020-06-17 13:26:33 +0200

    Ændringer i algoritmen.

--- Algorithm.py
+++ Algorithm.py
@@ -4,21 +4,25 @@ import Agent
 All return types need to be replaced by their equivalent method.
 """
 def decision(moves):
-    # Draw card is no moves available.
+    # Draw card if no moves available.
     if not moves:
-        return "DRAW CARD"
+        return "Træk et kort"
 
     # Check suit moves.
-    for move in moves:
-        if move.position == 8:
-            return "MOVE TO FIRST SUIT"
-        elif move.position == 9:
-            return "MOVE TO SECOND SUIT"
-        elif move.position == 10:
-            return "MOVE TO THIRD SUIT"
-        elif move.position == 11:
-            return "MOVE TO FOURTH SUIT"
-
+    for i in len(moves):
+        if moves[i][0][1] == 7:
+            return "Ryk kort til første suit"
+        elif moves[i][0][1] == 8:
+            return "Ryk kort til andet suit"
+        elif moves[i][0][1] == 9:
+            return "Ryk kort til tredje suit"
+        elif moves[i][0][1] == 10:
+            return "Ryk kort til fjerde suit"
+    """
+    Prioriteter:
+    1. Bunker med flest kort med forsiden nedad.
+    2. Hvis man rykker en kolonne så skal der prioriteres dem med konger.
+    """
     # Check regular moves.
     for move in moves:
         # TODO: Prioritér kort?
