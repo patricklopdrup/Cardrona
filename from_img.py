e71faf5fa5fcdd5b70ebec36575457775001172a
Patrick <patrick.lopdrup@gmail.com>
2020-06-18 13:46:26 +0200

    kan tage input fra ML og lave et spil kort i python console

--- from_img.py
+++ from_img.py
@@ -32,7 +32,7 @@ def parse_suit(input: str) -> str:
 
 def make_stock_pile(input_list: list):
     """ Parse the card that is turned from the stock pile and call draw_from_stock() """
-    top_card = input_list[0]
+    top_card = input_list[0][0]
     # Blank paper is seen as None
     if top_card is not None:
         m_card = card.Card(parse_value(top_card),
@@ -43,11 +43,11 @@ def make_stock_pile(input_list: list):
 
 def make_suit_pile(input_list: list):
     """ Parse the four suit piles from input_list """
-    for card_i in input_list:
+    for card_index, card_i in enumerate(input_list):
         # Blank paper is seen as None
         if card_i is not None:
             m_card = card.Card(parse_value(
-                card_i), parse_suit(card_i), None, -1, -1)
+                card_i[card_index][0]), parse_suit(card_i[card_index][0]), None, -1, -1)
             # Add the card to the suit pile in-game
             game.m_suit_pile.add_card(m_card)
 
@@ -64,7 +64,7 @@ def make_seven_column(input_list: list):
         # Default above_card as None
         above_card = None
         # If column in inner-list is not None or there exist facedown cards in the column
-        if col is not None or game.col_facedown[index_col] > 0:
+        if col[0] is not None or game.col_facedown[index_col] > 0:
             # Loops the col_facedown aka. the amount of cards facing down in each column
             for facedown in range(game.col_facedown[index_col]):
                 # Creates a default card facing down (value and suit does NOT matter)
@@ -76,8 +76,8 @@ def make_seven_column(input_list: list):
             # card_index is the index of the card in the inner-list
             for card_index, input_card in enumerate(col):
                 # Creates the card from inner-list. Also with x- and y-pos and above_card
-                m_card = card.Card(parse_value(input_card),
-                                   parse_suit(input_card),
+                m_card = card.Card(parse_value(input_card[0]),
+                                   parse_suit(input_card[0]),
                                    above_card,
                                    index_col,
                                    (card_index + game.col_facedown[index_col]))
