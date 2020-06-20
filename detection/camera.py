c77ac9f92498656ddbd160a830a813afad811a1e
Martin Rylund <martin@rylund.eu>
2020-06-20 17:27:47 +0200

    Added documentation to camera file

--- detection/camera.py
+++ detection/camera.py
@@ -2,12 +2,16 @@ import urllib.request
 import yaml
 from os import path
 
+# Global configuration for file
 cur_path = path.dirname(path.abspath(__file__))
 cfg_path = cur_path + "/cfg/cfg.yml"
 config = yaml.safe_load(open(cfg_path))
 
 
 def take_picture(num):
+    """
+    A simple function to take a picture from a given URL and saving it locally
+    """
     URL = config['camera_url']
 
     with urllib.request.urlopen(URL) as url:
