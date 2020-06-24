809c80d17f2e34d54dccaac5c105fac069931d41
Martin Rylund <martin@rylund.eu>
2020-06-24 03:47:06 +0200

    Added a file for general functions in automatic test (w/ Athu)

---
+++ automated_test/general_funcs.py
@@ -0,0 +1,9 @@
+def get_full_suit_name(suit):
+    if suit == "C":
+        return "Clubs"
+    elif suit == "D":
+        return "Diamonds"
+    elif suit == "H":
+        return "Hearts"
+    elif suit == "S":
+        return "Spades"
