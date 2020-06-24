9e12d2f5d2406c2b2c7835f9dc0ac70eb687becb
Martin Rylund <martin@rylund.eu>
2020-06-24 05:10:56 +0200

    Added actions to game loop in automated test (w/ Athu)

--- automated_test/game.py
+++ automated_test/game.py
@@ -1,12 +1,12 @@
 from pprint import pprint
 from game import game_columns as game_logic
-from make_game_test import show_test
 from game import card
 import numpy as np
 import yaml
 import cv2
 from automated_test import image_generation as ig
 from automated_test import general_funcs as gf
+from agent import action_moves
 from detection import detect as detector
 from agent import Agent
 from agent import Algorithm
@@ -41,7 +41,7 @@ class game:
 
         self.face_down = []
 
-        self.talon = draw_pile.Stock_pile()
+        self.talon = draw_pile.Stock_pile(self.game_data, self.game_data.m_suit_pile)
 
         self.detector = detector.detect()
 
@@ -63,8 +63,6 @@ class game:
         self.game_data.m_suit_pile = from_img.make_suit_pile(suit)
         self.game_data.solitaire = from_img.make_seven_column(cols, self.game_data)
 
-        pprint(self.game_data.solitaire)
-
         any_illegal = False
         for col in range(len(self.game_data.solitaire)):
             for m_card in self.game_data.solitaire[col]:
@@ -79,21 +77,79 @@ class game:
     def game_loop(self):
         # while True:
         # Current stage - What stage number this is
+        self.current_stage += 1
         img_path = f'{save_folder}/{self.current_stage}.jpg'
-        self.detector.load_state(img_path)
-        self.__load_from_detected_img()
+        try:
+            self.detector.load_state(img_path)
+            self.__load_from_detected_img()
+        except gf.ColumnNotLegal:
+            print("En kolonne var ikke legal, prøv igen")
+            input("Klik ENTER for nyt billede")
 
-        moves = Agent.all_possible(game_columns1=self.game_data, top_talon=self.talon)
+        moves = Agent.all_possible(self.game_data, self.talon)
         ai_answer = Algorithm.decision(moves)
         print(ai_answer.user_text)
 
-        pprint(self.detector.get_tableau(1))
+        to_card = ai_answer.to_card
+        from_card = ai_answer.from_card
+        action = ai_answer.pc_action
+
+        if action == action_moves.waste_to_col:
+            if to_card:
+                self.talon.move_to_column(to_card.x_pos)
+            else:
+                self.talon.move_to_column()
+        elif action == action_moves.waste_to_suit:
+            self.talon.move_to_suit_pile()
+        elif action == action_moves.col_to_suit:
+            self.game_data.move_to_suit_pile(
+                from_card.x_pos, from_card.y_pos)
+        elif action == action_moves.col_to_col:
+            self.game_data.move_in_game(from_card.x_pos, from_card.y_pos, to_card.x_pos)
+
+        show_test(self.game_data, self.talon)
+
+        self.generator.generate_image(game_data=self.game_data,
+                                      stage=self.current_stage, talon=self.talon)
 
     def test(self):
-        generator = ig.image_generation()
-        temp_card = self.deck.pop(0)
-        self.game_data.solitaire[0, self.game_data.get_pile_size_in_col(
-            0)] = temp_card
-        talon = self.deck.pop(0)
-        generator.generate_image(game_data=self.game_data, stage=self.current_stage, talon=talon)
+        self.generator = ig.image_generation()
+        # temp_card = self.deck.pop(0)
+        # self.game_data.solitaire[0, self.game_data.get_pile_size_in_col(
+        #     0)] = temp_card
+        self.generator.generate_image(game_data=self.game_data,
+                                      stage=1, talon=self.talon)
         self.game_loop()
+
+
+def show_test(game_object=None, talon=None):
+    """ Print the game """
+    if game_object:
+        local_game = game_object
+    else:
+        local_game = game
+    print("Waste pile:", talon.waste)
+
+    suit_piles = local_game.m_suit_pile.suit_piles
+    print("H:", *suit_piles['H'])
+    print("S:", *suit_piles['S'])
+    print("D:", *suit_piles['D'])
+    print("C:", *suit_piles['C'])
+
+    print()
+    for row in range(19):
+        print()
+        value = None
+        for col in range(7):
+            if local_game.solitaire[col, row] != 0:
+                value = 1
+                # Print back-side of card if it's flipped - else print the card
+                if local_game.solitaire[col, row] is None or local_game.solitaire[col, row].is_facedown:
+                    print("[ ]", end=" ")
+                else:
+                    print(local_game.solitaire[col, row], end=" ")
+            else:
+                print(" " * 4, end="")
+        if not value:
+            break
+    print()
